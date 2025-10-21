3bee0907b9b2c913fddc17bcc49f593ef6c2bd03
Lempereurjulien <julien.lempereur@epitech.eu>
2025-10-21 23:18:45 +0200

    plugin $pluginVersion add

--- savePlugin.sh
+++ savePlugin.sh
@@ -1,10 +1,10 @@
 !#bin/bash
 
-$pluginVersion = 1.0.0
+$pluginVersion="1.0.0"
 #1. Copier target *.jar dans minecraft-server/plugins
 
 cp "target/visualGame-$pluginVersion.jar" "/home/lempereur/Documents/Projets/minecraft/minecraft-server/plugin/"
-echo "✅ Copie du plugin effectué"
+echo "✅ Copie du plugin effectué $HOME"
 git add .
 echo "✅ Ajout du fichier au git"
 git commit -m'plugin $pluginVersion add'
